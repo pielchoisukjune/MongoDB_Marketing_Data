1f83a99cd505976913f4bac76cf1c54ff11021fb
pielchoisukjune <john@pielkorea.com>
2020-08-07 14:14:02 +0900

    Update report_common.js

--- Server/script/report_common.js
+++ Server/script/report_common.js
@@ -639,8 +639,8 @@
 		<div class="card">
 			<div class="image">
 				<!=THUMNAIL_CONTENTS=!>
-				<!=IFRAME_CONTENTS=!>
 			</div>
+			<!=IFRAME_CONTENTS=!>
 			<div class="content">
 					
 				<div style='font-size : 12px; font-family: "Noto Sans KR", sans-serif;'><!=TITLE=!></div>
