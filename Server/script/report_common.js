190de4db30c396dda18fcc030826cc5b28e8a176
pielchoisukjune <john@pielkorea.com>
2020-08-07 13:20:26 +0900

    .

--- Server/script/report_common.js
+++ Server/script/report_common.js
@@ -640,8 +640,8 @@
 			<div class="image">
 				<!=THUMNAIL_CONTENTS=!>
 			</div>
-			<!=IFRAME_CONTENTS=!>
 			<div class="content">
+				<!=IFRAME_CONTENTS=!>
 				<div style='font-size : 12px; font-family: "Noto Sans KR", sans-serif;'><!=TITLE=!></div>
 				<div class="meta" style='font-size : 12px;font-family: "Noto Sans KR", sans-serif;'>
 					<br>
@@ -757,10 +757,10 @@
 			var _html = '<h3  class="ui left aligned header">' + s + ' ( ' + so.length +  ' 건 )</h3>';
 				_html += '<div class="ui grid">';
 				_html += '<div class="sixteen wide column">';
-				_html += '<div class="ui four stackable cards">';
+				//_html += '<div class="ui four stackable cards">';
 				_html += window.PIEL.REPORT.make_marketing_list_html( so )
 				_html += '</div>';
-				_html += '</div>';
+				//_html += '</div>';
 				_html += '</div>';
 				r += _html;
 		}
