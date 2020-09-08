7e0012555072add13b2f024bd802ccd47d4d0a3e
pielchoisukjune <john@pielkorea.com>
2020-09-08 18:26:04 +0900

    Update report_common.js

--- Server/script/report_common.js
+++ Server/script/report_common.js
@@ -489,7 +489,7 @@
 
 		for( s in o1 ){
 			so = o1[ s ];
-			debugger;
+			//debugger;
 			var _click_col = "";
 			var _view_col = "";
 
@@ -500,7 +500,8 @@
 				.replace( "<!=COMMENT=!>", so['설명'] )
 				.replace( "<!=CNT_VIEW=!>", _view_col )
 				.replace( "<!=CNT_CLICK=!>", _click_col )
-				.replace( "<!=KEYWORD=!>", o0[ s ][ "키워드" ] )
+			if(	o0[ s ][ "키워드" ] ) _html1 = _html1 + _tStr00.replace( "<!=KEYWORD=!>", o0[ s ][ "키워드" ] )
+			else if( so[ "검페인 유형" ] == "쇼핑" ) _html1 = _html1 + _tStr00.replace( "<!=KEYWORD=!>", "구글 쇼핑 광고" )
 		}
 
 		r = _tStr.replace( "<!=CONTENTS=!>", _html1 )
