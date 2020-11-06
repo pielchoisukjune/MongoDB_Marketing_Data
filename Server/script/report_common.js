0724ec693f8c934877104777a578ed1c24c0dda5
pielchoisukjune <john@pielkorea.com>
2020-11-06 13:28:44 +0900

    Update report_common.js

--- Server/script/report_common.js
+++ Server/script/report_common.js
@@ -920,7 +920,16 @@
 		for( s in data ){
 			so = data[ s ];
 			var _html = '<h3  class="ui left aligned header">' + s + ' ( ' + so.length +  ' 건 )</h3>';
-				r += window.PIEL.REPORT.make_marketing_list_html_table( so )
+				_html += '<div class="ui three column grid">';
+//				_html += '<div class="three column row">'
+//				_html += '<div class="sixteen wide column">';
+//				_html += '<div class="ui four stackable cards">';
+				_html += window.PIEL.REPORT.make_marketing_list_html_table( so )
+//				_html += '</div>';
+//				_html += '</div>';
+//				_html += '</div>';
+				_html += '</div>';
+				r += _html;
 		}
 
 		tDom.innerHTML = r;
