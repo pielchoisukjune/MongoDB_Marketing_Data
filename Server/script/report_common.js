b7d98805fd337806883469c1f5a41925b03f57ea
pielchoisukjune <john@pielkorea.com>
2020-11-06 12:27:49 +0900

    Update report_common.js

--- Server/script/report_common.js
+++ Server/script/report_common.js
@@ -771,7 +771,7 @@
 			if( window.marketing.report.varihope[ "202010" ][ postId ] )
 			{
 				debugger;
-				var thumb_url = thumb.replace( "<!=THUMBNAIL=!>", window.marketing.report.varihope[ "202010" ].attachments.data[ 0 ].media.image.src );
+				var thumb_url = thumb.replace( "<!=THUMBNAIL=!>", window.marketing.report.varihope[ "202010" ][ postId ].attachments.data[ 0 ].media.image.src );
 				console.log( window.marketing.report.varihope[ "202010" ].attachments.data[ 0 ].media.image.src )
 				var iframe_url = "";
 			}
