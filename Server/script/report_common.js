83a08b722b82c94e291b2ab0be2f1e4e19fa9fe2
pielchoisukjune <john@pielkorea.com>
2020-11-16 15:39:29 +0900

    Update report_common.js

--- Server/script/report_common.js
+++ Server/script/report_common.js
@@ -271,9 +271,11 @@
 				var mpa_chart_loader = window.document.getElementById( "map_chart_loader" );
 				map_chart_loader.classList.remove("active");
 				//var imgUri = geochart.getImageURI();
-				window.charts || window.charts = {}
-				window.charts.map || window.charts.map = {}
-				window.charts.map.facebook || window.charts.map.facebook = geochart
+				if( !window.charts ) window.charts = {}
+				if( !window.charts.map ) window.charts.map || window.charts.map = {}
+				
+				window.charts.map.facebook = geochart;
+				
 				return;
 			});
 			geochart.draw(data, opts);	
