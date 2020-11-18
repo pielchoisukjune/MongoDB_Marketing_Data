122e06a295ae8761fc39f35fd757af98fb934762
pielchoisukjune <john@pielkorea.com>
2020-11-18 18:03:44 +0900

    .

--- Server/script/aheads_render_202010.js
+++ Server/script/aheads_render_202010.js
@@ -1,13 +1,15 @@
 
-window.addEventListener('DOMContentLoaded', function( e ){ window.PIEL.REPORT.getData() });
-window.document.getElementById( "downloadPdf" ).addEventListener('click',function(e){
-	window.PIEL.REPORT.loader.on();
-
-	var downloadFileNm = "report_" + window.PAGE_SETTING._BRAND_NM_ + "_" + window.PAGE_SETTING._TARGET_YEAR_ + window.PIEL.REPORT.pad( window.PAGE_SETTING._TARGET_MONTH_,2) + ".pdf";
-	var uri = "https://github.com/pielchoisukjune/reportToHTMLToPDF/raw/main/report/" + window.PAGE_SETTING._BRAND_NM_ + "/" + window.PAGE_SETTING._TARGET_YEAR_ + window.PIEL.REPORT.pad( window.PAGE_SETTING._TARGET_MONTH_,2) + "/" + downloadFileNm;
+(function(){
+	window.addEventListener('DOMContentLoaded', function( e ){ window.PIEL.REPORT.getData() });
+	window.document.getElementById( "downloadPdf" ).addEventListener('click',function(e){
+		window.PIEL.REPORT.loader.on();
 	
-	setTimeout(function(){
-		window.PIEL.REPORT.downloadURI( uri, downloadFileNm );
-		window.PIEL.REPORT.loader.off()
-	},2000)
-})
+		var downloadFileNm = "report_" + window.PAGE_SETTING._BRAND_NM_ + "_" + window.PAGE_SETTING._TARGET_YEAR_ + window.PIEL.REPORT.pad( window.PAGE_SETTING._TARGET_MONTH_,2) + ".pdf";
+		var uri = "https://github.com/pielchoisukjune/reportToHTMLToPDF/raw/main/report/" + window.PAGE_SETTING._BRAND_NM_ + "/" + window.PAGE_SETTING._TARGET_YEAR_ + window.PIEL.REPORT.pad( window.PAGE_SETTING._TARGET_MONTH_,2) + "/" + downloadFileNm;
+		
+		setTimeout(function(){
+			window.PIEL.REPORT.downloadURI( uri, downloadFileNm );
+			window.PIEL.REPORT.loader.off()
+		},2000)
+	})
+})()
