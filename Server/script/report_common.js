da08859ebd4cbeddc7e844c449f254b90835a25a
pielchoisukjune <john@pielkorea.com>
2020-11-19 16:55:49 +0900

    .

--- Server/script/report_common.js
+++ Server/script/report_common.js
@@ -1186,8 +1186,8 @@
 		var s,so;
 		for( s in data ){
 			so = data[ s ];
-			var _html = '<div  class="sixteen wide column"><div style="padding:20px 0px;"><h3  class="ui left aligned header">' + s + ' ( ' + so.length +  ' 건 )</h3>/div></div>';
-				_html += '<div  class="sixteen wide column">'
+			var _html = '<div class="sixteen wide column"><div style="padding:20px 0px;"><h3  class="ui left aligned header">' + s + ' ( ' + so.length +  ' 건 )</h3></div></div>';
+				_html += '<div class="sixteen wide column">'
 				_html += window.PIEL.REPORT.make_marketing_list_html_table( so, dateObj, brandNm )
 				_html += '</div>'
 				r += _html;
