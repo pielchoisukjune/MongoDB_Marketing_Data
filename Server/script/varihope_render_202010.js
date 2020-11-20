60275fda6062e321c888fbe5a7a086ce6378d130
pielchoisukjune <john@pielkorea.com>
2020-11-20 17:58:54 +0900

    .

--- Server/script/varihope_render_202010.js
+++ Server/script/varihope_render_202010.js
@@ -19,14 +19,16 @@
 			return;
 		}
 		console.log( "window.PIEL.REPORT Loaded." );
-		
+        
 		var target_month = window.PAGE_SETTING._TARGET_MONTH_ +"월";
-		
+        
+        window.PIEL.REPORT.insertLogo();
+        
 		if( window.document.getElementById("target_month") )
 		{
 			var title_month = window.document.getElementById("target_month");
-			title_month.innerText = target_month;
-			title_month.style.color = "rgba(255, 255, 255,10)";
+			title_month.innerText = window.PAGE_SETTING._TARGET_YEAR_ + "." + window.COMMON.URIL.pad( window.PAGE_SETTING._TARGET_MONTH_, 2 );
+			title_month.style.color = "rgba(0, 0, 0,10)";
 		}
 	
         window.PIEL.REPORT.drawTable__monthly_marketing_plan( "monthly_planning", data.statistic_monthly, target_month );
