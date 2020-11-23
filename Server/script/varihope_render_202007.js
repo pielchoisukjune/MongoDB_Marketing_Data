8510d28945e566c4b771a4d393b89ba0fc69e678
pielchoisukjune <john@pielkorea.com>
2020-11-23 18:44:45 +0900

    .

--- Server/script/varihope_render_202007.js
+++ Server/script/varihope_render_202007.js
@@ -79,7 +79,7 @@
         // EVENT;
         //--------------------------------------------------;
         window.document.getElementById( "downloadPdf" ).addEventListener('click',function(e){
-            window.PIEL.REPORT.loader.on();
+            window.PIEL.REPORT.loader.on( "PDF 파일을 생성중입니다." );
         
             var downloadFileNm = "report_" + window.PAGE_SETTING._BRAND_NM_ + "_" + window.PAGE_SETTING._TARGET_YEAR_ + window.PIEL.REPORT.pad( window.PAGE_SETTING._TARGET_MONTH_,2) + ".pdf";
             var uri = "https://github.com/pielchoisukjune/reportToHTMLToPDF/raw/main/report/" + window.PAGE_SETTING._BRAND_NM_ + "/" + window.PAGE_SETTING._TARGET_YEAR_ + window.PIEL.REPORT.pad( window.PAGE_SETTING._TARGET_MONTH_,2) + "/" + downloadFileNm;
